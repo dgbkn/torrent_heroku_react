240af9cfd32af8003a3684a24ba123956b3a32f9
dgbkn <anandrambkn@gmail.com>
2022-04-01 16:56:33 +0530

    asdad

--- src/components/CustomError.js
+++ src/components/CustomError.js
@@ -4,7 +4,7 @@ export const NoContentFound = (props) => {
   return (
     <div>
       <div className="d-flix justify-content-center">
-        <span className="h1">No Content Found</span>
+        <span className="h1">No Content Found..</span>
       </div>
     </div>
   );
